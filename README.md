be485a097efadbdc331566e77071bdd71a2fdf35
TECH-A56 <tech-a56@TECH-A56s-MacBook-Air.local>
2018-06-04 15:35:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+#Break down of goal to a few smaller tasks:
+
+1. Define rules
+2. Structure the game
+3. Draw the board
+	1. Draw fields
+	2. Plant mines
+	3. Calculate distances
+7. Find a way to traverse the board
+8. Implement the "reveal" logic
+
+
+##Defining the rules
+
+1. The goal of the game is to find all mines on the board.
+
+2. You reveal mines by clicking the board fields.
+3. If you reveal a field with mine you lose the game.
+4. If you reveal field without a mine it will show exact number of mines surrounding that field.
+5. If you reveal field without number it means that there are no mines in its surroundings. In that case board will reveal all connected empty fields with its surroundings.
+6. You can flag field by right-clicking it.
+7. If you click on a revealed field and you already flagged all mines around that field, board will reveal rest of the hidden fields. Of course, if you misplaced flags you will reveal a field with a mine and lose the game.
+
+## Boards
+- __Beginner__: 
+	- 9 X 9
+	- 10 mines (%12)
+- __Intermediate__:
+	- 16 X 16
+	- 40 mines (%16)
+- __Expert__:
+	- 30X16
+	- 99 mines (%20)
+
+
+<!--## Structure
+- Game
+	- startGame()
+- Board
+-->
---
+++ js/app.js
@@ -0,0 +1,189 @@
+// $(function() {
+//
+//   var mode = {
+//     beginner: [9, 9],
+//     intermediate: [16, 16],
+//     expert: [30, 16]
+//   }
+//
+//   var currentBoard = 'beginner';
+//
+//   for (var i = 0; i < mode[currentBoard][0].length; i++) {
+//     console.log(i);
+//   }
+//
+// });
+
+$(document).ready(function () {
+  var mode = 'beginner';
+  newGame(mode);
+  // $('#new-game').hide();
+
+  $('#mode li').click(function() {
+    $('#mode li').removeClass('selected');
+    $(this).addClass('selected');
+    mode = $(this).attr('id');
+    newGame(mode);
+    // $('#new-game').hide();
+  });
+
+  // $('#new-game').click(function() {
+  //     var mode = $('#mode li.selected').attr('id');
+  //     newGame(mode);
+  //     $('#new-game').hide();
+  // });
+
+  function newGame(mode) {
+    var board;
+    switch (mode) {
+      case 'intermediate':
+        board = new boardGenerator(16, 16);
+        break;
+      case 'expert':
+        board = new boardGenerator(16, 30);
+        break;
+      case 'beginner':
+      default:
+        board = new boardGenerator(9,9);
+        break;
+    }
+    board.render();
+    board.gameOver = false;
+
+    $('.space').click(function (event) {
+      board.click(event.target);
+    });
+    // return board;
+  }
+
+  // Board Object Generator
+  function boardGenerator(row, col){
+    this.row = row;
+    this.col = col;
+    this.spaces = [];
+    this.gameOver = false;
+    this.spacesCleared = 0;
+    this.bombCount = 0;
+
+    //Initializing the Object
+    this.spaces = new Array(this.row);
+    for (i = 0; i < this.row; i++) {
+        this.spaces[i] = new Array(this.col);
+        for (j = 0; j < this.col; j++) {
+            this.spaces[i][j] = new cell(false, 0);
+        }
+    }
+
+    var min = 1;
+    var max = this.row * this.col;
+    this.bombCount = bombNum();
+    $('#value').html(this.bombCount);
+    for (var i = 0; i < this.bombCount; i++) {
+      var bombIndex = Math.round(Math.random() * (max - 1));
+      var x = Math.floor(bombIndex / this.col);
+      var y = bombIndex % this.col;
+      this.spaces[x][y] = new cell(false, -1);
+    }
+    // for (var i = 0; i < this.row; i++) {
+    //   for (var j = 0; j < this.col; j++) {
+    //       this.spaces[i][j].holds = numBombNear.call(this, i, j);
+    //   }
+    // }
+
+
+    this.click = function (target_element) {
+      var row = $(target_element).attr("data-row");
+      var col = $(target_element).attr("data-col");
+      // if (this.gameOver === true) {
+      //     return;
+      // }
+      // if (this.spaces[row - 1][col - 1].explored == true) {
+      //     return;
+      // }
+      if (this.spaces[row - 1][col - 1].holds == -1) {
+        this.explode();
+        // } else if (this.spaces[row - 1][col - 1].holds == 0) {
+        //     this.clear(row - 1, col - 1);
+        //     uncoverSurroundings.call(this, row - 1, col - 1);
+        // } else {
+        //     this.clear(row - 1, col - 1);
+      }
+    }
+
+    this.render = function() {
+      var spaces = "";
+      for (var i = 1; i <= row; i++) {
+          for (var j = 1; j <= col; j++) {
+              spaces = spaces.concat('<div class="space" data-row="' + i + '" data-col="' + j + '">&nbsp;</div>');
+          }
+          spaces = spaces.concat('<br>');
+      }
+      $('#board').empty();
+      $('#board').append(spaces);
+    }
+
+    this.clear = function (row, col) {
+      var dom_target = 'div[data-row="' + (row + 1) + '"][data-col="' + (col + 1) + '"]';
+      $(dom_target).addClass('safe');
+      if (this.spaces[row][col].holds > 0) {
+        $(dom_target).text(this.spaces[row][col].holds);
+      } else {
+        $(dom_target).html('&nbsp');
+      }
+      checkAllCellsExplored.call(this);
+      this.spacesCleared++;
+      this.spaces[row][col].explored = true;
+    }
+
+    this.explode = function() {
+      for (i = 0; i < this.row; i++) {
+        for (j = 0; j < this.col; j++) {
+          if (this.spaces[i][j].holds == -1) {
+            var dom_target = 'div[data-row="' + (i + 1) + '"][data-col="' + (j + 1) + '"]';
+            $(dom_target).addClass('bomb');
+            $(dom_target).html('<i class="fa fa-bomb"></i>');
+          }
+        }
+      }
+      this.gameOver = true;
+      $('#new-game').show();
+    }
+
+  }
+
+  //cell Object
+  function cell(explored, holds){
+    this.explored = explored;
+    this.holds = holds;
+  }
+
+  //Bomb Count Generator
+  function bombNum() {
+    var num = 0;
+    switch(mode) {
+      case 'beginner':
+        num = 10;
+        break;
+      case 'intermediate':
+        num = 40;
+        break;
+      case 'expert':
+        num = 99;
+        break;
+    }
+    return num;
+  }
+
+  function randomArray(count, max) {
+    var arr = []
+    while(arr.length < count){
+      var randomnumber = Math.floor(Math.random()*100) + 1;
+      if(arr.indexOf(randomnumber) > -1){
+        continue;
+      }
+      arr[arr.length] = randomnumber;
+    }
+    return arr;
+  }
+
+});
